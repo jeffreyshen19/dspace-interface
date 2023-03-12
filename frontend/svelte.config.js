6e22b84878b041bbef272bcb3e504dc40eea7b5e
Jeffrey Shen <jjshen@mit.edu>
2023-03-12 10:16:01 -0400

    update vercel config

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import autoPreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
