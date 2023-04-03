06420d12a11450b09a0a477dca4fbaddde24dc8f
Jeffrey Shen <jjshen@mit.edu>
2023-04-03 14:39:01 -0400

    test random seed issue

--- frontend/src/routes/survey/entrance/+page.svelte
+++ frontend/src/routes/survey/entrance/+page.svelte
@@ -9,7 +9,7 @@
     async function submitEntranceSurvey(e){
         const formData = new FormData(e.target);
         const dataToInsert = Object.fromEntries(formData.entries());
-        const goalTaskTopics = ["Supply chain management", "Venture capital markets in Europe", "Air traffic control"];
+        const goalTaskTopics = ["Supply chain management", "Air traffic control", "Venture capital markets in Europe"];
 
         dataToInsert["is_control"] = Math.random() > 0.5; // Randomly assign to control or experimental
         dataToInsert["goal_task_first"] = Math.random() < 0.5; // Randomly assign order of goal task / non-goal gask
