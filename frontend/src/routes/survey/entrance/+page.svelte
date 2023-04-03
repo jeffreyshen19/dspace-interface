36cd1a0c55f1bc6d9442ce5e836e700368e97d8f
Jeffrey Shen <jjshen@mit.edu>
2023-04-03 17:01:00 -0400

    update seed

--- frontend/src/routes/survey/entrance/+page.svelte
+++ frontend/src/routes/survey/entrance/+page.svelte
@@ -9,7 +9,7 @@
     async function submitEntranceSurvey(e){
         const formData = new FormData(e.target);
         const dataToInsert = Object.fromEntries(formData.entries());
-        const goalTaskTopics = ["Supply chain management", "Air traffic control", "Venture capital markets in Europe"];
+        const goalTaskTopics = ["Air traffic control", "Venture capital markets in Europe", "Supply chain management"];
 
         dataToInsert["is_control"] = Math.random() > 0.5; // Randomly assign to control or experimental
         dataToInsert["goal_task_first"] = Math.random() < 0.5; // Randomly assign order of goal task / non-goal gask
